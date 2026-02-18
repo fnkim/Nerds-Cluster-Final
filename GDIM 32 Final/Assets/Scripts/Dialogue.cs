3b0e9f8f2e3282fa757b219b2f05825a1869e9f7
rbcfeng <rebeccf2@uci.edu>
2026-02-17 19:33:55 -0800

    Added dialogue for traveller and witch in a c# script

---
+++ GDIM 32 Final/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+
+/*
+
+Dialogue script:
+
+Traveller: *rumble* Man, I’m so hungry…
+
+I really need to eat…
+I don’t know how far the seaside town is…
+
+Ughhhh… there must be something edible around here…
+
+Woah! Am I imagining things, or is this a candyland?
+
+Whose house is this, and why does it look so tasty?
+
+Witch: Back off! *casts a spell*
+
+	What do you think you’re doing?
+
+Traveller: Sorry, just hungry…
+		*notice*
+		This your house?
+
+Witch: For course! You’re not welcome to eat it!
+You humans are so disgusting. You guys think you can go around eating everything.
+
+Traveller: Oh yeah? And why shouldn’t I eat it? Why shouldn’t I eat YOU too?
+
+Witch: It’s just disrespectful!
+
+Traveller: Give me a reason why I shouldn’t eat you.
+
+Witch: Well… it’s against the candyland law. 
+Traveller: Who cares? No one’s gonna know.
+	*rumble* Ugghh, I’m so hungry I could eat a house… and a witch…
+
+Witch: How ‘bout I cook something up for you?
+
+Traveller: Sure, why not.
+	I feel like a berry tart.
+	A good tart with lots of berries on it. Why don’t you make something like that?
+
+Witch: Sure, as long as you don’t eat my property and me. Sit here and I’ll make that for you.
+
+End cutscene
+
+
+
+
+
+Press [E] to interact
+
+Traveller: I feel like a berry tart right now… Hurry up and make it for me or you won’t see the Sun again.
+
+
+
+Press [E] to interact (correct order)
+
+Traveller: Yoooo, this is so fire. *eats*
+
+Witch: :)
+
+Press [E] to interact (incorrect order)
+
+Traveller: YOU BUBBLEGUM DUM DUM WITCH!! What happened to my order? Why do you and your house look so appetizing right now?
+
+Witch: What? NOOOOOO!!!
+
+
+*/
+
+}
+
+
+
+
+
+
