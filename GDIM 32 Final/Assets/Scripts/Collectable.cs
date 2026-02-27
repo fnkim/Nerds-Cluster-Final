7440abd24098c583101a9eda97f02585bb3e840f
lpher <lpher@uci.edu>
2026-02-27 01:07:27 -0800

    Made lemons disappear instead of whole bush when collected

--- GDIM 32 Final/Assets/Scripts/Collectable.cs
+++ GDIM 32 Final/Assets/Scripts/Collectable.cs
@@ -6,14 +6,31 @@ public class Collectable : MonoBehaviour, IInteractable
 {
     [SerializeField] private BushType type;
     [SerializeField] private int amount = 1;
+
     [SerializeField] private string promptText = "[E] Pick up";
-    public string PromptText => promptText;
+    public string PromptText => _collected ? "" : promptText;
+
     [SerializeField] private Transform promptAnchor;
     public Transform PromptAnchor => promptAnchor;
+
+    [SerializeField] private GameObject visualToDestroy;
+
+    private bool _collected;
+
     public void Interact(PlayerInteractor interactor)
     {
+        if (_collected) return;
+        _collected = true;
+
         Debug.Log($"Collected: {type} x{amount}");
-        Destroy(gameObject);
+
+        if (visualToDestroy != null)
+            Destroy(visualToDestroy);
+
+        var col = GetComponent<Collider>();
+        if (col != null) col.enabled = false;
+
+        Destroy(this);
     }
 }
 
