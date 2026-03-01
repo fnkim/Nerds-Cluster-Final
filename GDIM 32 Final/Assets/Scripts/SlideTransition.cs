165a27e392f85c4f7ed48605a713800e0101fbdf
fnkim <fannykim42@gmail.com>
2026-03-01 10:29:42 -0800

    Added the Intro Cutscene and set up Scene transitions

---
+++ GDIM 32 Final/Assets/Scripts/SlideTransition.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SlideTransition : MonoBehaviour
+{
+    public  List<GameObject> slidesList = new List<GameObject>();
+    public int selectedSlide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            slidesList.Add(child.gameObject);
+        }
+
+        selectedSlide = 0;
+        slidesList[selectedSlide].SetActive(true);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void MovetoNextSlide()
+    {
+        if(selectedSlide == 10)
+        {
+            SceneManager.LoadScene("Main");
+        }
+        else
+        {
+            selectedSlide ++;
+            slidesList[selectedSlide].SetActive(true);
+        }
+
+
+    }
+
+
+
+
+}
