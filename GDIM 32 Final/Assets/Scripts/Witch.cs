35059f3c0345540ce4169165cc5ac387c396de1f
fnkim <fannykim42@gmail.com>
2026-03-02 19:28:41 -0800

    edited the candyland cutscene images and changed some player animation stuff

--- GDIM 32 Final/Assets/Scripts/Witch.cs
+++ GDIM 32 Final/Assets/Scripts/Witch.cs
@@ -2,16 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum PlayerState
-{
-    Walking,
-    Talking,
-    Interacting
-}
 
 public class Witch : MonoBehaviour
 {
-    PlayerState _currentActivity;
+
     [SerializeField] private CharacterController controller;
     [SerializeField] private float speed = 6f;
     [SerializeField] private float turnSmoothTime = 0.1f;
@@ -21,50 +15,32 @@ public class Witch : MonoBehaviour
     {
 
 
-
-       _currentActivity = PlayerState.Walking;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_currentActivity == PlayerState.Walking)
-        {
-            animator.SetBool("Talking", false);
-            animator.SetBool("Interacting", false);
 
 
-            //movement stuff
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        //movement stuff
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-            if (direction.magnitude >= 0.1f)
-                {
-                    float targetAngle = Mathf.Atan2(- direction.x, - direction.z) * Mathf.Rad2Deg;
-                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                    transform.rotation = Quaternion.Euler(0f, angle, 0f);
-                    controller.Move(direction * speed * Time.deltaTime);
-                    animator.SetBool("Walking", true);
-                }
-                else
-                {
-                    animator.SetBool("Walking", false);
-                }
+        if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(- direction.x, - direction.z) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                controller.Move(direction * speed * Time.deltaTime);
+                animator.SetBool("Walking", true);
+            }
+            else
+            {
+                animator.SetBool("Walking", false);
 
-
-
-
-        }else if (_currentActivity == PlayerState.Talking)
-        {
-            animator.SetBool("Walking", false);
-            animator.SetBool("Talking", true);
-        }else if (_currentActivity == PlayerState.Interacting)
-        {
-            animator.SetBool("Walking", false);
-            animator.SetBool("Interacting", true);
         }
+            
     }
-
 
 }
